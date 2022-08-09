992de4ba3f00e463e94111269e6093aca59ae366
Christopher Barnes <chris.barnes.2000@me.com>
2022-08-09 12:54:29 -0700

    [Update]: CrossOrigin

--- src/main/java/com/revature/controllers/ProductController.java
+++ src/main/java/com/revature/controllers/ProductController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/product")
 @CrossOrigin(
-  origins = "*",
+  origins = http://aac53e81081b042258fc80efa94a009c-104931072.us-east-1.elb.amazonaws.com:3000/,
   allowCredentials = "true"
 )
 public class ProductController {
