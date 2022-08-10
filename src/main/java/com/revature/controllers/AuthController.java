671459fd65ec152e7dc2ffdc04ec8a0cfbe118b8
Christopher Barnes <chris.barnes.2000@me.com>
2022-08-10 15:13:43 -0700

    [Update]: CorsOrigin Frontend

--- src/main/java/com/revature/controllers/AuthController.java
+++ src/main/java/com/revature/controllers/AuthController.java
@@ -17,8 +17,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://adebcf51e12fe4acc9f4deac43840e35-1727142081.us-east-1.elb.amazonaws.com:5000", allowCredentials = "true")
-// @CrossOrigin(origins = "http://a541fc074149e4506816882a4c7ef8e6-623895046.us-east-1.elb.amazonaws.com:3000", allowCredentials = "true")
+@CrossOrigin(origins = "http://a541fc074149e4506816882a4c7ef8e6-623895046.us-east-1.elb.amazonaws.com:3000", allowCredentials = "true")
 public class AuthController{
 
     private final AuthService authService;
--- src/main/java/com/revature/controllers/PodMonitoringController.java
+++ src/main/java/com/revature/controllers/PodMonitoringController.java
@@ -6,8 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/test")
-// @CrossOrigin(origins = "http://a541fc074149e4506816882a4c7ef8e6-623895046.us-east-1.elb.amazonaws.com:3000", allowCredentials = "true")
-@CrossOrigin(origins = "http://adebcf51e12fe4acc9f4deac43840e35-1727142081.us-east-1.elb.amazonaws.com:5000", allowCredentials = "true")
+@CrossOrigin(origins = "http://a541fc074149e4506816882a4c7ef8e6-623895046.us-east-1.elb.amazonaws.com:3000", allowCredentials = "true")
 public class PodMonitoringController {
 
 	static String hostname = System.getenv("HOSTNAME");
